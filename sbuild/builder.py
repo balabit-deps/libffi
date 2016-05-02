fa802fbf39401c4911eca302f335d93794d19757
Tibor Benke <tibor.benke@balabit.com>
2016-05-02 13:20:56 +0200

    sbuild: don't use default configure opts
    libffi doesn't need any configure opts.
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- sbuild/builder.py
+++ sbuild/builder.py
@@ -1,12 +1,4 @@
 from AutotoolsBuilder import AutotoolsBuilder
 
 def get_builder():
-    return AutotoolsBuilder(get_default_config_opts(), extra_env=None)
-
-def get_default_config_opts():
-    try:
-        import configure_opts
-    except ImportError:
-        return None
-    else:
-        return configure_opts.default_opts
+    return AutotoolsBuilder(default_configure_opts=None, extra_env=None)
